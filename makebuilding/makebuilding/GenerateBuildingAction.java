4928653e7101864de8da8a304163850ccf6b3d52
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2024-09-27 16:54:29 -0500

    Update GenerateBuildingAction.java

--- makebuilding/makebuilding/GenerateBuildingAction.java
+++ makebuilding/makebuilding/GenerateBuildingAction.java
@@ -5,6 +5,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,6 +19,7 @@ import java.util.Random;
 import java.awt.geom.Point2D;
 import java.util.Set;
 
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.MainApplication;
@@ -36,22 +38,26 @@ import org.openstreetmap.josm.data.validation.tests.CrossingWays;
 import org.openstreetmap.josm.tools.Geometry;
 import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.RemoveNodesCommand;
+import org.openstreetmap.josm.data.coor.LatLon;
 
 import jtsjosm.JtsJosmAction;
 
 public class GenerateBuildingAction extends JosmAction {
 
-    public GenerateBuildingAction() {
-            super(tr("Generate Buildings To Highway"),
-                        "Generate Buildings To Highway",
-                        tr("Generate Buildings To Highway"),
-                        Shortcut.registerShortcut(
-                            "generatebuildings:tohighway",
-                            "Generate buildings to highway",
-                            java.awt.event.KeyEvent.VK_B,
-                            Shortcut.CTRL_SHIFT
-                        ),
-                        false);
+    private BuildingDialogAction buildingDialog;
+
+    public GenerateBuildingAction(BuildingDialogAction buildingDialog) {
+        super(tr("Generate Buildings To Highway"),
+                    "Generate Buildings To Highway",
+                    tr("Generate Buildings To Highway"),
+                    Shortcut.registerShortcut(
+                        "generatebuildings:tohighway",
+                        "Generate buildings to highway",
+                        java.awt.event.KeyEvent.VK_B,
+                        Shortcut.CTRL_SHIFT
+                    ),
+                    false);
+        this.buildingDialog = buildingDialog;
 
     }
 
@@ -114,9 +120,9 @@ public class GenerateBuildingAction extends JosmAction {
 
     public List<Way> iterativeBuildings(Way w) {
 
-        double avgWidth = 7.5;
-        double avgDepth = 10;
-        double avgSetback = 2;
+        double avgWidth = buildingDialog.width;
+        double avgDepth = buildingDialog.depth;
+        double avgSetback = buildingDialog.setback;
         JtsJosmAction jtsAction = new JtsJosmAction();
 
         Way owr = jtsAction.offsetWay(w, 10, true);
@@ -128,108 +134,211 @@ public class GenerateBuildingAction extends JosmAction {
         List<Way> ways = new ArrayList<Way>();
 
         // Right side first
-        while (cumulativeDist < rightLength) {
-            List<Node> nodes = new ArrayList<Node>();
-            // Set front nodes
-            nodes.add(jtsAction.interpolate(owr, cumulativeDist));
-            cumulativeDist += avgWidth*(Math.random()+0.5);
-            nodes.add(jtsAction.interpolate(owr, cumulativeDist));
-            // Set back nodes
-            // Get angle between nodes 0 & 1
-            angle = getAngle(nodes.get(0), nodes.get(1));
-            // Set a random depth
-            depth = avgDepth*(Math.random()+0.5);
-            // Translate node 0 & 1 by depth & angle
-            dx = depth*Math.cos(angle + Math.PI/2);
-            dy = depth*Math.sin(angle + Math.PI/2);
-            nodes.add(jtsAction.translateNode(nodes.get(1), dx, dy));
-            nodes.add(jtsAction.translateNode(nodes.get(0), dx, dy));
-            nodes.add(nodes.get(0));
+        if (buildingDialog.rightSide) {
+            while (cumulativeDist < rightLength) {
+                List<Node> nodes = new ArrayList<Node>();
+                // Set front nodes
+                nodes.add(jtsAction.interpolate(owr, cumulativeDist));
+                cumulativeDist += avgWidth*(Math.random()+0.5);
+                nodes.add(jtsAction.interpolate(owr, cumulativeDist));
+                // Set back nodes
+                // Get angle between nodes 0 & 1
+                angle = getAngle(nodes.get(0), nodes.get(1));
+                // Set a random depth
+                depth = avgDepth*(Math.random()+0.5);
+                // Translate node 0 & 1 by depth & angle
+                dx = depth*Math.cos(angle + Math.PI/2);
+                dy = depth*Math.sin(angle + Math.PI/2);
+                nodes.add(jtsAction.translateNode(nodes.get(1), dx, dy));
+                nodes.add(jtsAction.translateNode(nodes.get(0), dx, dy));
+                nodes.add(nodes.get(0));
 
-            // Add nodes to way
-            Way way = new Way();
-            way.setNodes(nodes);
-            way.put("building", "yes");
-            ways.add(way);
+                // Add nodes to way
+                Way way = new Way();
+                way.setNodes(nodes);
+                Way previousWay = new Way();
+                if(ways.size() > 1){
+                    previousWay = ways.get(ways.size()-1);
+                }
+                if (jtsAction.overlaps(way, previousWay)) {
+                    Way newWay;
+                    // 50 / 50 union vs difference
+                    if (Math.random() > 0.75) {
+                        newWay = jtsAction.subtract(way, previousWay);
+                    } else {
+                        newWay = jtsAction.union(previousWay, way);
+                        // Remove the old way
+                        ways.remove(previousWay);
+                    }
+                    newWay.put("building", "yes");
+                    ways.add(newWay);
+                } else {
+                    way.put("building", "yes");
+                    ways.add(way);
+                }
+                cumulativeDist += buildingDialog.lotSpacing;
 
+            }
         }
         // Reset
         cumulativeDist = 0;
 
-        while (cumulativeDist < leftLength) {
-            List<Node> nodes = new ArrayList<Node>();
-            // Set front nodes
-            nodes.add(jtsAction.interpolate(owl, cumulativeDist));
-            cumulativeDist += avgWidth*(Math.random()+0.5);
-            nodes.add(jtsAction.interpolate(owl, cumulativeDist));
-            // Set back nodes
-            // Get angle between nodes 0 & 1
-            angle = getAngle(nodes.get(0), nodes.get(1));
-            // Set a random depth
-            depth = avgDepth*(Math.random()+0.5);
-            // Translate node 0 & 1 by depth & angle
-            dx = -1*depth*Math.cos(angle + Math.PI/2);
-            dy = -1*depth*Math.sin(angle + Math.PI/2);
-            nodes.add(jtsAction.translateNode(nodes.get(1), dx, dy));
-            nodes.add(jtsAction.translateNode(nodes.get(0), dx, dy));
-            nodes.add(nodes.get(0));
+        if (buildingDialog.leftSide) {
+            while (cumulativeDist < leftLength) {
+                List<Node> nodes = new ArrayList<Node>();
+                // Set front nodes
+                // double setbackVariance = 3*(Math.random()-0.5);
+                nodes.add(jtsAction.interpolate(owl, cumulativeDist));
+                cumulativeDist += avgWidth*(Math.random()+0.5);
+                nodes.add(jtsAction.interpolate(owl, cumulativeDist));
+                // Set back nodes
+                // Get angle between nodes 0 & 1
+                angle = getAngle(nodes.get(0), nodes.get(1));
+                // Set a random depth
+                depth = avgDepth*(Math.random()+0.5);
+                // Translate node 0 & 1 by depth & angle
+                dx = -1*depth*Math.cos(angle + Math.PI/2);
+                dy = -1*depth*Math.sin(angle + Math.PI/2);
+                nodes.add(jtsAction.translateNode(nodes.get(1), dx, dy));
+                nodes.add(jtsAction.translateNode(nodes.get(0), dx, dy));
+                nodes.add(nodes.get(0));
 
-            // Add nodes to way
-            Way way = new Way();
-            way.setNodes(nodes);
-            way.put("building", "yes");
-            ways.add(way);
-
+                // Add nodes to way
+                Way way = new Way();
+                way.setNodes(nodes);
+                Way previousWay = new Way();
+                if(ways.size() > 1){
+                    previousWay = ways.get(ways.size()-1);
+                }
+                if (jtsAction.overlaps(way, previousWay)) {
+                    Way newWay;
+                    // 25 / 75 union vs difference
+                    if (Math.random() > 0.75) {
+                        newWay = jtsAction.subtract(way, previousWay);
+                    } else {
+                        newWay = jtsAction.union(previousWay, way);
+                        // Remove the old way
+                        ways.remove(previousWay);
+                    }
+                    newWay.put("building", "yes");
+                    ways.add(newWay);
+                } else {
+                    way.put("building", "yes");
+                    ways.add(way);
+                }
+                cumulativeDist += buildingDialog.lotSpacing;
+            }
         }
-
         return ways;
-
     }
 
+    public int getGridHash(Way way, int gridX, int gridY) {
+        // Get the bounding box
+
+        // Subdivide bounding box into smaller boxes gridX x gridY wide
+
+        // Assign the way a box hash given its centroid
+
+
+        return 0;
+    }
+
+    public boolean buildingObstructsWithWaysFast(Way building, Collection<Way> ways) {
+        // Get the x displacement and y displacement of the building
+
+        // Assign each way in ways a hash based on the x & y displacements
+
+        // Get the adjacent hashes
+
+        // Get the ways in those adjacent hashes
+
+        // Check if a building obstructs with any of the ways
+        return false;
+    }
+
+    public boolean intersects(Way w1, Way w2) {
+
+        List<Node> nodes1 = w1.getNodes();
+        List<Node> nodes2 = w2.getNodes();
+
+        Geometry.PolygonIntersection pi = Geometry.polygonIntersection(nodes1, nodes2);
+
+        if (pi == Geometry.PolygonIntersection.OUTSIDE) {
+            return false;
+        }
+        return true;
+    }
+
+    public Node getCentroid(Way way) {
+        if (way == null || way.getNodesCount() == 0) {
+            return null; // Handle case where way is null or has no nodes
+        }
+
+        double sumLat = 0.0;
+        double sumLon = 0.0;
+
+        for (Node node : way.getNodes()) {
+            sumLat += node.getCoor().lat();
+            sumLon += node.getCoor().lon();
+        }
+
+        double centroidLat = sumLat / way.getNodesCount();
+        double centroidLon = sumLon / way.getNodesCount();
+
+        return new Node(new LatLon(centroidLat, centroidLon));
+    }
+
+    boolean buildingObstructsWithWays(Way building, SpatialHashGrid hashGrid) {
+        JtsJosmAction jtsAction = new JtsJosmAction();
+
+        List<Way> nearbyWays = hashGrid.getAdjacentWays(building);
+
+        for (Way way : nearbyWays) {
+            if (building == way) {
+                continue;
+            }
+            if (jtsAction.overlaps (building, way)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
 
         // Get selected ways
         DataSet ds = MainApplication.getLayerManager().getEditDataSet();
         Collection<Way> selected = ds.getSelectedWays();
         Collection<Command> commands = new ArrayList<Command>();
+        Node center = new Node(MainApplication.getMap().mapView.getCenter());
+        SpatialHashGrid hashGrid = new SpatialHashGrid(center, 30, 1000, 1000);
 
         // Get selected way
         float angle = 0;
 
-        for (Way selectedWay : selected) {
+        for (Way way : selected) {
             // Generate parallel nodes of the way
-            List<Node> parallelNodes = generateParallelNodes(selectedWay);
+            // List<Node> parallelNodes = generateParallelNodes(way);
 
-            List<Way> buildings = new ArrayList<Way>(parallelNodes.size()-2);
+            List<Way> buildings = new ArrayList<Way>();
             int index = 0;
             double width, length, setback;
-            // for (Node node : parallelNodes) {
-            //     index++;
-            //     // Skip if node is the first item
-            //     if (index == 0) {
-            //         continue;
-            //     }
-            //     // End if it is the last item
-            //     if (index == parallelNodes.size() - 1 ) {
-            //         break;
-            //     }
-            //     // Get the angle of the node before and after
-            //     angle = getAngle(parallelNodes.get(index-1), parallelNodes.get(index+1));
 
-            //     // Generate a rectangle about the node and at the angle
-            //     width = 7.5 * (Math.random()+.5);
-            //     length = 10 * (Math.random()+.5);
-            //     setback = 5 * (Math.random() - 0.5);
+            buildings.addAll(iterativeBuildings(way));
 
-            //     buildings.add(generateBuilding(node, width, length, angle, setback));
-            // }
-
-            for (Way way : selected) {
-                buildings.addAll(iterativeBuildings(way));
+            // Add all buildings to hash grid
+            for(Way building : buildings) {
+                hashGrid.addToHash(building);
             }
 
             // Add nodes of the building to dataset
             for (Way building : buildings) {
+                if (buildingObstructsWithWays(building, hashGrid)) {
+                    // 50 / 50 whether to union it or difference it
+
+                }
+
                 List<Node> nodes = building.getNodes();
                 System.out.println("------------------" + nodes);
                 for (int i = 0; i < nodes.size() - 1; i++) {
@@ -242,7 +351,6 @@ public class GenerateBuildingAction extends JosmAction {
                 commands.add(new AddCommand(ds, building));
             }
         }
-
 
         SequenceCommand sequenceOfCommands = new SequenceCommand("Generate buildings", commands, true);
         UndoRedoHandler.getInstance().add(sequenceOfCommands);
